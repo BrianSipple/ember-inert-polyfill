1114b31a49794f73a4b7fc863a13d28894d8226d
Brian Sipple <Bsipple57@gmail.com>
2016-08-19 06:28:12 -0700

    Add version parity test (#1)
    Add version parity test

--- blueprints/ember-inert-polyfill/index.js
+++ blueprints/ember-inert-polyfill/index.js
@@ -4,6 +4,6 @@ module.exports = {
   normalizeEntityName: function() {},
   
   afterInstall: function() {
-    return this.addBowerPackageToProject('inert-polyfill', '0.2.1');
+    return this.addBowerPackageToProject('inert-polyfill', '^0.2.1');
   }
 };
